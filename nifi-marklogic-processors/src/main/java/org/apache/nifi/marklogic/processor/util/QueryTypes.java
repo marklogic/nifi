864d785187d776dc92ef0431daeb8036bf4e5b20
Rob Rudin <rob.rudin@marklogic.com>
2022-11-22 15:47:28 -0500

    DEVEXP-87: Improving docs for Query, Delete, and ApplyTransform
    Added some dynamic property descriptions that were missing too

--- nifi-marklogic-processors/src/main/java/org/apache/nifi/marklogic/processor/util/QueryTypes.java
+++ nifi-marklogic-processors/src/main/java/org/apache/nifi/marklogic/processor/util/QueryTypes.java
@@ -5,7 +5,7 @@ import org.apache.nifi.components.AllowableValue;
 public class QueryTypes {
     public static final String COLLECTION_STR = "Collection Query";
     public static final AllowableValue COLLECTION = new AllowableValue(COLLECTION_STR, COLLECTION_STR,
-        "Comma-separated list of collections to query from a MarkLogic server");
+        "Comma-separated list of collections to query");
     public static final String COMBINED_JSON_STR = "Combined Query (JSON)";
     public static final AllowableValue COMBINED_JSON = new AllowableValue(COMBINED_JSON_STR, COMBINED_JSON_STR,
         "Combine a string or structured query with dynamic query options; also supports JSON serialized CTS queries");
@@ -14,7 +14,7 @@ public class QueryTypes {
         "Combine a string or structured query with dynamic query options; also supports XML serialized CTS queries");
     public static final String STRING_STR = "String Query";
     public static final AllowableValue STRING = new AllowableValue(STRING_STR, STRING_STR,
-        "A Google-style query string to search documents and metadata.");
+        "A string that utilizes the MarkLogic search grammar for expressing a query");
     public static final String STRUCTURED_JSON_STR = "Structured Query (JSON)";
     public static final AllowableValue STRUCTURED_JSON = new AllowableValue(STRUCTURED_JSON_STR,
         STRUCTURED_JSON_STR,
